d1f7fcc91345e5ce13a743007f4c28d44480be8a
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-01-30 10:19:57 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log('hello');
+
+let a = 2;
+let b = 4;
+
+let sum = 2 + 4;
+let mult = 2 * 8;
+let div = 2 / 4;
+let mod = 2 % 4;
+
+console.log(sum, mult, div, mod);
+
+let arr1 = [2, 3, 5, 7];
+let arr2 = [2, 3, 5, 7];
+
+let c = arr1[0] * arr2[3];
+
+console.log(c);
+
+let arrNew = arr1.pop();
+console.log('arrNew', arrNew);
+console.log('arr1', arr1);
